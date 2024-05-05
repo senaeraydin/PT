830820728d28cd145a3a8555c1aec57f46675da2
senaeraydin <senaeraydin2@gmail.com>
2024-05-05 14:35:37 +0200

    committed

--- LibraryTask1_/DataTest/DataTest.cs
+++ LibraryTask1_/DataTest/DataTest.cs
@@ -10,7 +10,7 @@ namespace DataTest
         {
             var users = new List<User>
             {
-                new User { UserId = "1", UserName = "user1", FirstName = "Sena", LastName = "Eraydýn", Email = "senaeraydin@gmail.com" },
+                new User { UserId = "1", UserName = "user1", FirstName = "Sena", LastName = "Eraydin", Email = "senaeraydin@gmail.com" },
                 new User { UserId = "2", UserName = "user2", FirstName = "Ece", LastName = "Atakol", Email = "eceatakol@gmail.com" },
                 new User { UserId = "3", UserName = "user3", FirstName = "Sinem", LastName = "Fidan", Email = "sinemfidan@gmail.com" }
             };
